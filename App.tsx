bb663364a427ed9c10b1ba3bf1568c48cee6cde7
Hassan-rgb1 <steamme310@gmail.com>
2025-02-03 12:06:28 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import Task16 from './Tasks/Task16';
+import Task17 from './Tasks/Task17';
+import Task18 from './Tasks/Task18';
+import Task19 from './Tasks/Task19';
+import Task20 from './Tasks/Task20';
+import Task21 from './Tasks/Task21';
+import Task22 from './Tasks/Task22';
+import Task23 from './Tasks/Task23';
+import Task24 from './Tasks/Task24';
+const App = () => {
+    return <Task24/>;
+};
+
+export default App;
